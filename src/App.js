7811f072cc71a8d44a04cd0ae4d7f114e7c4f19d
yasaman rezaeeshirsavar <yasaman@dineeasy.ca>
2021-07-05 20:31:53 +0430

    add alll files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+// import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <br />
+      <br />
+      <br />
+      Application up and running
+    </div>
+  );
+}
+
+export default App;
